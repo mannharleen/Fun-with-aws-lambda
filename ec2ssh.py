51ee887e71b128583d234d0d1965e2fdb9a3d2bd
Ubuntu <mannharleen@gmail.com>
2017-12-20 09:28:22 +0000

    parametrized host and other stuff

--- ec2ssh.py
+++ ec2ssh.py
@@ -2,20 +2,23 @@ import boto3
 import paramiko
 
 hosts = ['10.29.5.184']
-commands = [        "ls",        "echo $JAVA_HOME"        ]
+commands = { "10.29.5.184" : ["screen -md /home/ubuntu/apache-hive-1.0.0-bin/bin/hive --service metastore", "ls" ] }
+source_key_locs = { "10.29.5.184" : "harleen/ssh/harleen-key-pair.pem" }
+target_key_locs = { "10.29.5.184" : "/tmp/harleen-key-pair.pem" }
+bucket_locs = { "10.29.5.184" : "modharleen" }
 
 def ssh_ex_commands(event, context):
     s3_client = boto3.client("s3") 
-    s3_client.download_file("modharleen","harleen/ssh/harleen-key-pair.pem","/tmp/harleen-key-pair.pem")    
-    k = paramiko.RSAKey.from_private_key_file("/tmp/harleen-key-pair.pem")
-    c = paramiko.SSHClient()
-    c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     for host in hosts:
+        s3_client.download_file(bucket_locs[host],source_key_locs[host],target_key_locs[host])    
+        k = paramiko.RSAKey.from_private_key_file(target_key_locs[host])
+        c = paramiko.SSHClient()
+        c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         print("Connecting to " + host)
         c.connect( hostname = host, username = "ubuntu", pkey = k )
         print("Connected to " + host)
-    for command in commands:
-        print ("Executing {}".format(command))
-        stdin , stdout, stderr = c.exec_command(command)
-        print (stdout.read())
-        print (stderr.read())
+        for command in commands[host]:
+            print ("Executing {}".format(command))
+            stdin , stdout, stderr = c.exec_command(command)
+            print (stdout.read())
+            print (stderr.read())
