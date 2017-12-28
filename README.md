3e98066f81541c2e9f5d5b15db9b9d79e14c7555
Ubuntu <mannharleen@gmail.com>
2017-12-28 02:46:12 +0000

    added ec2 into startEC2script

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # awslambda
+The git repo contains my code used on AWS lambda. Most of these codes are being used in our production environment with minor tweeks.
--- start-stop-ec2/startEC2Instance.py
+++ start-stop-ec2/startEC2Instance.py
@@ -1,6 +1,17 @@
 import boto3
+#
+# Current version - v1.2
+# Last modified on - 28-12-17
+# Last modified by - Harleen Mann
+# Created by - Harleen Mann
+# Description - This script is called by cloudwatch trigger based on the schedule time
+# Notes
+    # v1.2: Added qlik-uat-env and talend-uat-env servers
+#
+#
 region = 'ap-southeast-1'
-instances = ['i-01d57c6e17451c2ef']
+instances = ['i-01d57c6e17451c2ef', 'i-0befd69a2f0c86cd3', 'i-0c9297af766518a54'] 
+# instance added in same order = centOS-harleen, qlikUAT, talendUAT
 
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2', region_name=region)
