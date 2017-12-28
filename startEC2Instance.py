668cce1935cbd760ce44daae7db324a87ed0b638
Ubuntu <mannharleen@gmail.com>
2017-12-28 02:32:26 +0000

    redo folders

--- startEC2Instance.py
+++
@@ -1,9 +0,0 @@
-import boto3
-region = 'ap-southeast-1'
-instances = ['i-01d57c6e17451c2ef']
-
-def lambda_handler(event, context):
-    ec2 = boto3.client('ec2', region_name=region)
-    ec2.start_instances(InstanceIds=instances)
-    print ('starting your instances: ' + str(instances))
-
--- stopEC2Instance.py
+++
@@ -1,8 +0,0 @@
-import boto3
-region = 'ap-southeast-1'
-instances = ['i-01d57c6e17451c2ef']
-
-def lambda_handler(event, context):
-    ec2 = boto3.client('ec2', region_name=region)
-    ec2.stop_instances(InstanceIds=instances)
-    print ('stopped your instances: ' + str(instances))
